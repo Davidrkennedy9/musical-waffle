34d18eeccdc1c95c302fb8f54e5fbfdab15758c2
David Kennedy <davidkennedy@Davids-MacBook-Air.local>
2025-02-07 14:21:05 -0700

    complete iteration1

--- lib/shelter.rb
+++ lib/shelter.rb
@@ -1,2 +1,18 @@
 class Shelter
+    attr_reader :location, :num
+
+    def initialize(location, num)
+        @location = location
+        @num = num
+    end
+    def name
+        @location
+    end
+    def capacity
+        @num = 5
+    end
+    def pets
+        pets = []
+    end
+
 end
